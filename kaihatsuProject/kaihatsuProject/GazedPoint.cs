63aa8b537ad69f40672878b96ed31b27e5714baa
sekiki <37245287+sekiki@users.noreply.github.com>
2019-09-03 02:21:29 +0900

    commit

--- kaihatsuProject/kaihatsuProject/GazedPoint.cs
+++ kaihatsuProject/kaihatsuProject/GazedPoint.cs
@@ -5,15 +5,149 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.IO;
 
 namespace kaihatsuProject
 {
+    class PointData
+    {
+        static Stopwatch myWatch;
+
+        public double x;
+        public double y;
+        public long time;
+        public PointData(double xv, double yv)
+        {
+            x = xv; y = yv;
+            time = myWatch.ElapsedMilliseconds;
+        }
+
+        static PointData()
+        {//staticメンバの初期化(C#だとstaticコンストラクタなるものを用いる)
+            myWatch = new Stopwatch();
+            myWatch.Start();
+        }
+
+        //public static long GetCurrentTime() { return myWatch.ElapsedMilliseconds; }
+    }
+
+    class TimeLinePointDatas : IMySetter
+    {
+        private LinkedList<PointData> PointDatas;
+        private long timeThres;//時間閾値
+        private double distanceThres;//距離閾値
+        private long baseTime;//この時間から閾値の時間まで見続けてたら興味あり判定
+        public bool StillGazingAtOnePoint { get; private set; }//興味検知の後から他に視線が移動するまでの間true
+        public bool InterestDetected { get; private set; }//興味検知がなされた瞬間だけtrue(振動モジュールで通知出すとき使う)
+
+
+        public TimeLinePointDatas()
+        {
+            PointDatas = new LinkedList<PointData>();
+            timeThres = 100;//ミリ秒
+            distanceThres = 30;//ラジアン
+            baseTime = 0;
+            StillGazingAtOnePoint = false;
+            InterestDetected = false;
+        }
+
+        public void Add(PointData pd)//listにAddするついでに興味検知してプロパティをアップデートする関数
+        {//ここを書き換えることで色々な興味検知の方法を試せるので改変したければここのPointDatas.AddLast(pd);以外を書き換えてください
+            int k = PointDatas.Count;
+            if (k > 1)
+            {
+                double d = Math.Pow((PointDatas.Last.Value.x - pd.x), 2) + Math.Pow((PointDatas.Last.Value.y - pd.y), 2);
+                d = Math.Sqrt(d);
+                if (d < distanceThres)//時間判定へ
+                {
+                    if (pd.time - baseTime > timeThres)
+                    {
+                        if (!StillGazingAtOnePoint)
+                        {//注視が始まってから最初にここに来た
+                            StillGazingAtOnePoint = true;
+                            InterestDetected = true;//「今」検知された
+                        }
+                        else
+                        {
+                            InterestDetected = false;
+                        }
+                    }
+                }
+                else
+                {
+                    baseTime = pd.time;
+                    StillGazingAtOnePoint = false;
+                    InterestDetected = false;
+                }
+            }
+            PointDatas.AddLast(pd);
+            if (k > 100)
+            {//要素数が無限に大きくなってメモリを圧迫しないよう適当なとこで消す
+                PointDatas.RemoveFirst();
+            }
+
+        }
+
+        public void WriteToCSV(string fileName)//csv出力する
+        {
+            int k = PointDatas.Count;
+            string str = "";
+            if (k > 1)
+            {
+                str = PointDatas.Last.Value.x.ToString() + "," + PointDatas.Last.Value.y.ToString() + "," + PointDatas.Last.Value.time.ToString() + "\n";
+            }
+            else
+            {
+                str = "TimeLinePointDatasクラスのPointDatas.countが0です" + "\n";
+            }
+            File.AppendAllText(fileName, str);
+        }
+
+        public LinkedList<PointData> GetPointDatas() { return PointDatas; }
+
+        public void LoadMember(List<string> memNames, List<string> memValues)
+        {//メモ帳からロードするLoadSettingsの関数にこのクラスをぶち込むために必要なインターフェース(暫定版)
+            for (int i = 0; i < memNames.Count; i++)
+            {
+                if ("timethres" == memNames[i].ToLower())
+                {
+                    try { timeThres = long.Parse(memValues[i]); }
+                    catch (Exception e) { MessageBox.Show(e.ToString()); }
+                    Console.WriteLine("TimeLinePointDatas 時間閾値 " + timeThres.ToString() + " ミリ秒が読み込まれました");
+                }
+                else if ("distancethres" == memNames[i].ToLower())
+                {
+                    try { distanceThres = double.Parse(memValues[i]); }
+                    catch (Exception e) { MessageBox.Show(e.ToString()); }
+                    Console.WriteLine("TimeLinePointDatas 角度閾値 " + distanceThres.ToString() + " 度が読み込まれました");
+                }
+            }
+        }
+
+        public void SaveMember(List<string> memNames, List<string> memValues)
+        {//未実装
+
+        }
+
+
+    }
+
+    /// <summary>
+    /// 視点座標を計算・格納し興味検知も行う, バインディング可なプロパティ有
+    /// </summary>
     class GazedPoint : INotifyPropertyChanged//バインディング用, 視点の座標を求めて格納するとviewに反映する
     {
+        public TimeLinePointDatas pointDatas;//テキストからロードするための暫定処理
+        public event MyEventHandler InterestDetected;//興味検知時に発火するイベント, 振動デバイスへの通知が入ればいい
+        private string gazeStateStr;
         private int xValue;
         private int yValue;
         public int X { get { return xValue; } }
         public int Y { get { return yValue; } }
+        private double SX;
+        private double SY;
 
         private int originXvalue;//原点(0度, 0度を受け取ったときに出てくる点), キャリブレーション用
         private int originYvalue;
@@ -41,21 +175,76 @@ namespace kaihatsuProject
             set { KyValue = value; RaisePropertyChanged("KyValue"); }
         }
 
+        //private bool isWritingCsvFlag;
+        public bool IsWriting { get; set; }
+
+        public string GazeStateLabel//バインディング用, 興味検知の状態を示す
+        {
+            get { return gazeStateStr; }
+            set
+            {
+                gazeStateStr = value;
+                RaisePropertyChanged("GazeStateLabel");
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
         public GazedPoint(int xo, int yo)
         {
             xValue = originXvalue = xo;
             yValue = originYvalue = yo;
             Kx = Ky = 400;
+            pointDatas = new TimeLinePointDatas();
+            IsWriting = false;
+            gazeStateStr = "";
+            SX = 0;
+            SY = 0;
         }
 
+        /// <summary>
+        /// 座標の再計算と興味検知の判定を行う
+        /// </summary>
+        /// <param name="angleX"></param>
+        /// <param name="angleY"></param>
         public void ReculculateXY(double angleX, double angleY)
         {//角度はdegree
-            xValue = (int)(originXvalue + Kx * Math.Sin(angleX * Math.PI / 180));
-            yValue = (int)(originYvalue + Ky * Math.Sin(angleY * Math.PI / 180));
-            RaisePropertyChanged("X");//UIに更新通知
+            xValue = (int)(originXvalue + Kx * Math.Sin(GazeFilterX(angleX) * Math.PI / 180));
+            yValue = (int)(originYvalue + Ky * Math.Sin(GazeFilterY(angleY) * Math.PI / 180));
+            RaisePropertyChanged("X");//UIに更新通知, なぜここに書く?
             RaisePropertyChanged("Y");//UIに更新通知
+            pointDatas.Add(new PointData(angleX, angleY));
+            if (pointDatas.InterestDetected)
+            {
+                //System.Windows.MessageBox.Show("興味検知");
+                //ここで振動モジュールに通知するための処理, 書くためにはイベントハンドラ持つ必要あり?
+                if (InterestDetected != null)
+                {
+                    MyEventArgs e = new MyEventArgs();
+                    InterestDetected(this, e);
+                }
+            }
+
+            if (pointDatas.StillGazingAtOnePoint)
+            {
+                GazeStateLabel = "興味検知中";
+            }
+            else
+            {
+                GazeStateLabel = "";
+            }
+
+            if (IsWriting)
+            {
+                pointDatas.WriteToCSV("gazeLog.csv");
+            }
         }
 
+        public void getHeatMap(ref System.Windows.Media.Imaging.BitmapSource Image)//HeatMapのbitmapを作成させて吐かせる
+        {
+            HeatMap.GenerateHeatMap(pointDatas.GetPointDatas(), ref Image);
+
+        }
 
         //以下がviewmodelのクラスには必要らしい
         public event PropertyChangedEventHandler PropertyChanged;
@@ -65,6 +254,25 @@ namespace kaihatsuProject
             var d = PropertyChanged;
             if (d != null)
                 d(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //指数移動平均で視点座標のばらつきを抑える関数
+        public double GazeFilterX(double angle)
+        {
+            double N = 9;
+            double alpha = 2 / (N + 1);
+
+            SX = alpha * angle + (1 - alpha) * SX;
+            return SX;
+        }
+
+        public double GazeFilterY(double angle)
+        {
+            double N = 9;
+            double alpha = 2 / (N + 1);
+
+            SY = alpha * angle + (1 - alpha) * SY;
+            return SY;
         }
     }
 }
--- kaihatsuProject/kaihatsuProject/MainWindow.xaml.cs
+++ kaihatsuProject/kaihatsuProject/MainWindow.xaml.cs
@@ -30,34 +30,45 @@ namespace kaihatsuProject
 
     public partial class MainWindow : System.Windows.Window//
     {//
-        NamedPipeCommunication pipe;
+        public NamedPipeCommunication pipe;
         ExternalAppManager myManager;
         GazedPoint gaze;
 
         VideoCaptureDevice webcam;
-        private FilterInfoCollection videoDevices;
+        FilterInfoCollection videoDevices;
+
+        SerialCommunication mySerial;
+
 
         public MainWindow()
-        {
-            InitializeComponent();
+        {//長い  けどスッキリさせるのが難しい...
+            //ログ出力のための設定
+            StreamWriter streamWriter = new StreamWriter("debug.log", false);//既にファイルがあっても追加ではなく上書き
+            streamWriter.AutoFlush = true;//これやっとかないとバッファに貯めこまれてアプリ終了まで書き込まれなかったりする
+            Console.SetOut(streamWriter);
+            Console.WriteLine("ウィンドウコンストラクタ");
 
-            this.DataContext = this;            
+            InitializeComponent();//これは必須
+
 
             pipe = new NamedPipeCommunication("mypipe");
-            pipe.getMessageEvent += test;
-            myCanvas2.DataContext = pipe;//パイプコンストラクト前に置くと機能しないしエラーも出ない
+            pipe.MessageReceived += test;//メッセージ受信時の処理
             pipe.WaitConnectionAsync();
+            testLabel.DataContext = pipe;//パイプコンストラクト前に置くと機能しないしエラーも出ない
+            messageLabel.DataContext = pipe;
 
             myManager = new ExternalAppManager();
-            LoadSettings.LoadFromTextTest<ExternalAppManager>("settings.txt", myManager);
+            LoadSettings.LoadFromTextTest<ExternalAppManager>("settings.txt", myManager);//テキストからロード
 
             gaze = new GazedPoint(300, 200);
-            Image1.DataContext = gaze;
-            myCanvas.DataContext = gaze;
+            gaze.InterestDetected += OnInterestDetected;
+            gazeImage.DataContext = gaze;
             text1.DataContext = gaze;
             text2.DataContext = gaze;
             text3.DataContext = gaze;
             text4.DataContext = gaze;
+            gazeStateLabel.DataContext = gaze;
+            LoadSettings.LoadFromTextTest<TimeLinePointDatas>("settings.txt", gaze.pointDatas);
 
             videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             comboBox1.Items.Clear();
@@ -70,16 +81,19 @@ namespace kaihatsuProject
             {
                 comboBox1.Items.Add(device.Name);
             }
-            comboBox1.SelectedIndex = 0; //make dafault to first cam            
+            comboBox1.SelectedIndex = 0; //make dafault to first cam
 
+            mySerial = new SerialCommunication("a", 1);//test
+            LoadSettings.LoadFromTextTest<SerialCommunication>("settings.txt", mySerial);
+            mySerial.Open();
+
+            Console.WriteLine("ウィンドウコンストラクタ終了");
         }
 
-        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            //Bitmap img = (Bitmap)eventArgs.Frame.Clone();
-            //var hbitmap = img.GetHbitmap();
-            //videoImage.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        //以下イベントハンドラ//////////////////////////////////////////////////////////////////////////////////////////////
 
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {//webカメラが新しいキャプチャを受け取ったときに発火
             Dispatcher.Invoke(new Action(() => {
                 using (var stream = new MemoryStream())
                 using (Bitmap img = (Bitmap)eventArgs.Frame.Clone())
@@ -87,14 +101,7 @@ namespace kaihatsuProject
                     img.Save(stream, ImageFormat.Bmp);
                     videoImage.Source = BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
                 }
-            }));            
-        }
-
-        private void Window_MouseMove(object sender, MouseEventArgs e)
-        {
-            //Point point = e.GetPosition(this);
-            //Canvas.SetLeft(Image1, point.X);
-            //Canvas.SetTop(Image1, point.Y            
+            }));
         }
 
         private void message_Click(object sender, RoutedEventArgs e)
@@ -102,47 +109,98 @@ namespace kaihatsuProject
             //MessageBox.Show("メッセージボックス");
             webcam = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
             webcam.NewFrame += video_NewFrame;
-            webcam.Start();            
+            webcam.Start();
         }
 
         private void swichApp_Click(object sender, RoutedEventArgs e)
-        {
+        {//連携アプリ起動/終了
             if (myManager.IsAlive) { myManager.Close(); }
             else { myManager.Start(); }
         }
 
         void test(object sender, MyEventArgs e)
         {//角度をパイプから受け取った時に発火する
-            //MessageBox.Show(e.message);
             try
             {
                 string[] str = e.message.Split(',');
-                gaze.ReculculateXY(double.Parse(str[0]), double.Parse(str[1]));
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    gaze.ReculculateXY(double.Parse(str[0]), double.Parse(str[1]));
+                }));
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 gaze.ReculculateXY(0, 0);
-            }            
-                       
-        }        
+            }
+            //Console.WriteLine("ヒートマップ開始");
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+            Dispatcher.Invoke(new Action(() =>
+            {
+                Bitmap bMapcolor = new Bitmap(600, 350);//bMapをカラーにした後情報を格納するビットマップ
+                System.Windows.Media.Imaging.BitmapSource Image;
+                IntPtr hbitmap = bMapcolor.GetHbitmap();
+                Image = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());//新たに追加
+                gaze.getHeatMap(ref Image);
+                HeatMapa.Source = Image;
+                DeleteObject(hbitmap);
+            }));
+
+            //Console.WriteLine("ヒートマップ終了");
+        }
+
+        void OnInterestDetected(object sender, MyEventArgs e)
+        {//興味推定時の処理, シリアル通信で振動デバイスに通知する処理を書く
+            //Console.WriteLine("視線検出 テスト");
+            mySerial.Write();
+        }
+
+        private void aaa_CLick(object sender, RoutedEventArgs e)
+        {//カメラ再取得, デモの動画撮るのに必要だっただけで今後消える
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            comboBox1.Items.Clear();
+            if (videoDevices.Count == 0)
+                throw new ApplicationException();
+            foreach (FilterInfo device in videoDevices)
+            {
+                comboBox1.Items.Add(device.Name);
+            }
+            comboBox1.SelectedIndex = 0; //make dafault to first cam
+        }
+
+        private void WriteToCSV_CLick(object sender, RoutedEventArgs e)
+        {//CSV出力モードにする/出力止める
+            if (!gaze.IsWriting)
+            {
+                gaze.IsWriting = true;
+                WriteToCSVButton.Content = "csv出力:出力中";
+            }
+            else
+            {
+                gaze.IsWriting = false;
+                WriteToCSVButton.Content = "csv出力:停止中";
+            }
+        }
+
+        private void Calibrate_CLick(object sender, RoutedEventArgs e)
+        {//
+
+        }
+
+        private void Window_Closing(object sender, EventArgs e)
         {//終了処理
-            //pipe.Close();
+            mySerial.Close();
             if (myManager.IsAlive)
             {
                 myManager.Close();
             }
-
-        }
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            //webcam.Stop();
             webcam.NewFrame -= video_NewFrame;
             webcam.SignalToStop();
             webcam = null;
-        }        
+        }
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject); // gdi32.dllのDeleteObjectメソッドの使用を宣言する。
     }
 }
 
